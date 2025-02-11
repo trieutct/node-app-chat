0a419344f2b888f7dd53cab31fa2be6ffbb61f24
trieutct <trinhcongtrieu2972002@gmail.com>
2025-02-11 16:34:47 +0700

    update end point

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`.green);
     connectD();
